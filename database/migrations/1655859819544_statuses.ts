59b880cdf49f36be639559519744c639bfa50205
Claudio Bertini Batista <bertini.b@icloud.com>
2022-06-22 01:43:47 -0300

    Create status migration

--- database/migrations/1655859819544_statuses.ts
+++ database/migrations/1655859819544_statuses.ts
@@ -5,13 +5,10 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.increments('id').primary()
+      table.string('name').unique().notNullable()
       table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('updated_at').nullable()
     })
   }
 
