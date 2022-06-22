63b5c37b3341da78736212c21f84a8a3f7dfbe1b
Claudio Bertini Batista <bertini.b@icloud.com>
2022-06-22 00:14:26 -0300

    Team Create Validator Created

---
+++ app/Validators/Team/CreateTeamValidator.ts
@@ -0,0 +1,19 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CreateTeamValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    name: schema.string([
+      rules.required(),
+      rules.alpha({ allow: ['space', 'underscore', 'dash'] }),
+    ]),
+    deparment: schema.number([rules.required()]),
+  })
+
+  public messages: CustomMessages = {
+    required: 'The {{ field }} is required to create a new team',
+    alpha: 'This {{ field }} cannot contain characters other than the letters of the alphabet.',
+  }
+}
